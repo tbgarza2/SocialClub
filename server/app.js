17695f79b62fbe2c66b5e4b7d2e1046db137e3e4
TehBakey <tehbakey@gmail.com>
2020-02-18 13:34:25 -0600

    comment out api router

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { apiRouter } = require('./api');
+// const { apiRouter } = require('./api');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/../react-client/dist`));
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
 
 module.exports = {
     app,
