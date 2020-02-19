04cf586633d18473da2484dcd85eabd046bc0f11
17gstyron <17gstyron@gmail.com>
2020-02-19 11:57:38 -0600

    fixed webpack build  Please enter the commit message for your changes. Lines starting

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, "client/dist/"),
     publicPath: "/dist/",
     filename: "bundle.js"
   },
