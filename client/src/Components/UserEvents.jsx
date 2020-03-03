8be6d83eca7ab23a084a4bc4e22c4e59fd363856
NaseerHines <Hinesnaseer@gmail.com>
2020-03-03 16:39:36 -0600

    Fixed BUKU linting errors client side

--- client/src/Components/UserEvents.jsx
+++ client/src/Components/UserEvents.jsx
@@ -2,23 +2,22 @@ import React, { Component } from 'react';
 
 
 class UserEvents extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-
-        return (
-            <div>
-                <ul>
-                    {this.props.events.map( event => (
-                        <li key={event.id} id={event.id} onClick={this.props.handleClick}>
-                            {event.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.events.map(event => (
+            <li key={event.id} id={event.id} onClick={this.props.handleClick}>
+              {event.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 export default UserEvents;
