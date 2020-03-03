852aef1c476fb1fb62663e6b5a163931cc9d65e4
Tommy Garza <tbgarza2@gmail.com>
2020-03-03 11:22:38 -0600

    added socketPath to mysql.configs

--- server/db/config.js
+++ server/db/config.js
@@ -1,8 +1,9 @@
-const { DB_HOST, DB_USER, DB_PASS } = require('./process');
+const { DB_HOST, DB_USER, DB_PASS, DB_PATH } = require('./process');
 
 module.exports = {
     host: DB_HOST || 'localhost',
     user: DB_USER || 'root',
     password: DB_PASS || '',
     database: 'socialclub',
+    socketPath: DB_PATH
 };
--- server/db/process.js
+++ server/db/process.js
@@ -2,10 +2,12 @@ const DB_HOST = 'team4ahalfstar'; // Added this as hostname
 const DB_USER = 'team4ahalfstar';
 const DB_PASS = 'team4ahalfstar';
 const DB_NAME = 'socialclub';
+const DB_PATH = `/cloudsql/${process.env.team4ahalfstar}`;
 
 module.exports = {
     DB_HOST,
     DB_USER,
     DB_PASS,
-    DB_NAME
+    DB_NAME,
+    DB_PATH
 }
