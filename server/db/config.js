14d2294dd857771fefb9fc71f7c91856f53f71b8
Tommy Garza <tbgarza2@gmail.com>
2020-03-03 14:46:17 -0600

    added full url and added password

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,9 @@
 // const { DB_HOST, DB_USER, DB_PASS, DB_PATH } = require('./process.js');
 
 module.exports = {
-    host: process.env.DB_HOST || 'team4ahalfstar.appspot.com',
+    host: process.env.DB_HOST || 'https://team4ahalfstar.appspot.com/',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
+    password: process.env.DB_PASS || 'team4ahalfstar',
     database: 'socialclub',
     socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}` || ''
 };
