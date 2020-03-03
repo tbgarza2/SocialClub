3bdaabcfa9b448ab71ef19b7d133edc202ade981
Thomas Garza <tbgarza2@gmail.com>
2020-03-03 15:59:35 -0600

    hardcoded values for connecting db to server

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,17 @@
 // const { DB_HOST, DB_USER, DB_PASS, DB_PATH } = require('./process.js');
 
+// module.exports = {
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASS || 'team4ahalfstar',
+//     database: 'socialclub',
+//     // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}` || '/cloudsql/team4ahalfstar:us-central1:team4ahalfstar'
+// };
+
 module.exports = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'team4ahalfstar',
+    host: 'localhost',
+    user: 'root',
+    password: 'team4ahalfstar',
     database: 'socialclub',
-    socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}` || '/cloudsql/team4ahalfstar:us-central1:team4ahalfstar'
+    socketPath: '/cloudsql/team4ahalfstar:us-central1:team4ahalfstar'
 };
