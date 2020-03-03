8b362470ccd6cfbf77f3454e14faf4bd1329bd78
Tommy Garza <tbgarza2@gmail.com>
2020-03-03 14:55:02 -0600

    changed host and password again (fingers crossed)

--- server/db/config.js
+++ server/db/config.js
@@ -1,7 +1,7 @@
 // const { DB_HOST, DB_USER, DB_PASS, DB_PATH } = require('./process.js');
 
 module.exports = {
-    host: process.env.DB_HOST || 'https://team4ahalfstar.appspot.com/',
+    host: process.env.DB_HOST || 'team4ahalfstar.appspot.com',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'team4ahalfstar',
     database: 'socialclub',
