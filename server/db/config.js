8ef8c28c83b5f3997839fbb06455f058f7643877
Tommy Garza <tbgarza2@gmail.com>
2020-03-03 14:18:23 -0600

    added specific host instead of localhost

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,9 @@
 // const { DB_HOST, DB_USER, DB_PASS, DB_PATH } = require('./process.js');
 
 module.exports = {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'team4ahalfstar.appspot.com',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     database: 'socialclub',
-    socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+    socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}` || ''
 };
