51895c455975a1ffc85fa9f0178e0c37dad3b985
Tommy Garza <tbgarza2@gmail.com>
2020-03-04 11:55:29 -0600

    hope this works for deployment

--- server/db/config.js
+++ server/db/config.js
@@ -1,11 +1,11 @@
 // const { DB_HOST, DB_USER, DB_PASS, DB_PATH } = require('./process.js');
 
 module.exports = {
-    // host: '34.70.53.155',
-    user: 'team4ahalfstar',
+    host: '34.70.53.155',
+    user: 'root',
     password: 'team4ahalfstar',
     database: 'socialclub',
-    socketPath: '/cloudsql/team4ahalfstar:us-central1:team4ahalfstar'
+    // socketPath: '/cloudsql/team4ahalfstar:us-central1:team4ahalfstar'
 };
 
 //   host: 'localhost',
