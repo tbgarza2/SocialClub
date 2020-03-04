5aa522d0ff3614ff99c0786d7e20f76f0e0ceb9f
NaseerHines <Hinesnaseer@gmail.com>
2020-03-04 11:50:42 -0600

    Fixed linter errors

--- server/api/messages.js
+++ server/api/messages.js
@@ -1,17 +1,18 @@
 const { Router } = require('express');
+
 const messageRouter = Router();
 const {
   getMessages,
-} = require('../db/index.js')
+} = require('../db/index.js');
 
 messageRouter.get('/messages/:id_sender/:id_recipient', (req, res) => {
   const { id_sender, id_recipient } = req.params;
   getMessages(id_sender, id_recipient)
     .then(messages => {
-      res.send(messages)
+      res.send(messages);
     });
 });
 
 module.exports = {
-  messageRouter
-}
+  messageRouter,
+};
