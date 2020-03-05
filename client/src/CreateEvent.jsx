28ac6f4c01dcee1eca1f8420799013cb6ed17bf6
Thomas Garza <tbgarza2@gmail.com>
2020-03-05 15:16:54 -0600

    removed changes to create event

--- client/src/CreateEvent.jsx
+++ client/src/CreateEvent.jsx
@@ -32,29 +32,30 @@ class CreateEvent extends React.Component {
       phones.forEach(phone => this.sendTwilio(phone));
     }
     const { name, address, date, category, summary } = this.state;
+    const { currentUser } = this.props;
     axios({
       method: 'post',
       url: 'api/event/events',
       data: {
-        name: name,
-        address: address,
-        date: date,
-        category: category,
-        summary: summary,
+        name,
+        address,
+        date,
+        category,
+        summary,
         creator: this.props.currentUser,
         roomID: name,
-      }
+      },
     });
 
-    axios({
-      method: 'post',
-      url: 'api/chatkit/rooms',
-      data: {
-        id: name,
-        creatorId: this.props.googleUser.profileObj.email,
-        name: name,
-      }
-    });
+    // axios({
+    //   method: 'post',
+    //   url: 'api/chatkit/rooms',
+    //   data: {
+    //     id: name,
+    //     creatorId: this.props.googleUser.profileObj.email,
+    //     name: name,
+    //   }
+    // });
   }
 
   sendTwilio(phone) {
@@ -119,88 +120,88 @@ class CreateEvent extends React.Component {
     return (
       <div>
         <form className="form-horizontal">
-                <fieldset>
+          <fieldset>
 
 
-                    <legend>Create Event:</legend>
+            <legend>Create Event:</legend>
 
 
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="eventname">Event Name</label>
-                        <div className="col-md-4">
-                            <input id="eventname" name="eventname" type="text" value={name} onChange={this.handleName} placeholder="" className="form-control input-md" required="" />
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="eventname">Event Name</label>
+              <div className="col-md-4">
+                <input id="eventname" name="eventname" type="text" value={name} onChange={this.handleName} placeholder="" className="form-control input-md" required="" />
 
-                          </div>
-                      </div>
-
-
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="address">Address</label>
-                        <div className="col-md-4">
-                            <input id="address" name="address" type="text" value={address} onChange={this.handleAddress} placeholder="" className="form-control input-md" required="" />
-
-                          </div>
-                      </div>
+              </div>
+            </div>
 
 
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="datetime">Date &amp; Time</label>
-                        <div className="col-md-4">
-                            <input id="datetime" name="datetime" type="datetime-local" value={date} onChange={this.handleDateTime} placeholder="" className="form-control input-md" required="" />
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="address">Address</label>
+              <div className="col-md-4">
+                <input id="address" name="address" type="text" value={address} onChange={this.handleAddress} placeholder="" className="form-control input-md" required="" />
 
-                          </div>
-                      </div>
-
-
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="category">Category</label>
-                        <div className="col-md-4">
-                            <select id="category" name="category" value={category} onChange={this.handleCategory} className="form-control">
-                                <option value="-">-</option>
-                                <option value="Social">Social</option>
-                                <option value="Gaming">Gaming</option>
-                                <option value="Food">Food</option>
-                                <option value="Sports">Sports</option>
-                              </select>
-                          </div>
-                      </div>
+              </div>
+            </div>
 
 
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="summary">Summary</label>
-                        <div className="col-md-4">
-                            <textarea className="form-control" id="summary" name="summary" value={summary} onChange={this.handleSummary}>A short description of your event!</textarea>
-                          </div>
-                      </div>
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="datetime">Date &amp; Time</label>
+              <div className="col-md-4">
+                <input id="datetime" name="datetime" type="datetime-local" value={date} onChange={this.handleDateTime} placeholder="" className="form-control input-md" required="" />
 
-                    {
-                      phones.map((phone, index) => (
-                        <div className="form-group" key={index}>
-                          <label className="col-md-4 control-label" htmlFor="to">Enter phone</label>
-                          <div className="col-md-4">
-                            <input style={{ borderRadius: 4 }} placeholder="(555) 555-5555" type="tel" name="to" id={index} value={phone} onChange={(e) => this.handlePhone(e, index)} />
-                            <button id="removephonebutton" name="removephonebutton" type="button" className="btn btn-danger btn-sm" onClick={(e) => this.removePhone(e, index)}>X</button>
-                          </div>
-                        </div>
-                      ))
-                    }
+              </div>
+            </div>
 
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="phonebutton"></label>
-                        <div className="col-md-4">
-                            <button id="phonebutton" name="phonebutton" className="btn btn-secondary" type="button" onClick={this.addPhone}>Invite friends</button>
-                          </div>
-                      </div>
 
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="singlebutton"></label>
-                        <div className="col-md-4">
-                            <button id="singlebutton" name="singlebutton" className="btn btn-primary" type="button" onClick={this.handleSubmit}>Sumbit</button>
-                          </div>
-                      </div>
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="category">Category</label>
+              <div className="col-md-4">
+                <select id="category" name="category" value={category} onChange={this.handleCategory} className="form-control">
+                  <option value="-">-</option>
+                  <option value="Social">Social</option>
+                  <option value="Gaming">Gaming</option>
+                  <option value="Food">Food</option>
+                  <option value="Sports">Sports</option>
+                </select>
+              </div>
+            </div>
 
-                  </fieldset>
-              </form>
+
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="summary">Summary</label>
+              <div className="col-md-4">
+                <textarea className="form-control" id="summary" name="summary" value={summary} onChange={this.handleSummary}>A short description of your event!</textarea>
+              </div>
+            </div>
+
+            {
+              phones.map((phone, index) => (
+                <div className="form-group" key={index}>
+                  <label className="col-md-4 control-label" htmlFor="to">Enter phone</label>
+                  <div className="col-md-4">
+                    <input style={{ borderRadius: 4 }} placeholder="(555) 555-5555" type="tel" name="to" id={index} value={phone} onChange={(e) => this.handlePhone(e, index)} />
+                    <button id="removephonebutton" name="removephonebutton" type="button" className="btn btn-danger btn-sm" onClick={(e) => this.removePhone(e, index)}>X</button>
+                  </div>
+                </div>
+              ))
+            }
+
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="phonebutton"></label>
+              <div className="col-md-4">
+                <button id="phonebutton" name="phonebutton" className="btn btn-secondary" type="button" onClick={this.addPhone}>Invite friends</button>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label className="col-md-4 control-label" htmlFor="singlebutton"></label>
+              <div className="col-md-4">
+                <button id="singlebutton" name="singlebutton" className="btn btn-primary" type="button" onClick={this.handleSubmit}>Sumbit</button>
+              </div>
+            </div>
+
+          </fieldset>
+        </form>
 
       </div>
     );
