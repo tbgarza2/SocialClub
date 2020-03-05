402ba96bfe1e737c22a7f50b59e73fa8c2adbdb8
NaseerHines <Hinesnaseer@gmail.com>
2020-03-05 15:17:33 -0600

    rsvp page fixed

--- client/src/Components/AttendingEvents.jsx
+++ client/src/Components/AttendingEvents.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-
-class AttendingEvents extends Component {
+class UserEvents extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      rsvpEvents: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getrsvpEvents();
+  }
+
+  getrsvpEvents() {
+    const { userId } = this.props;
+    axios(`/api/rsvp/rsvp/${userId}`)
+      .then(res => {
+        console.log("grabbed your RSVP'd");
+        this.setState({ rsvpEvents: res.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
+    const { handleClick } = this.props;
     return (
       <div>
-        <p>Hello World</p>
+        <h3>Your RSVP Events</h3>
+        <ul>
+          {this.state.rsvpEvents.map(event => (
+            <li key={event.id} id={event.id} onClick={handleClick}>
+              {event.name}
+            </li>
+          ))}
+        </ul>
+        {/* HelloWorld */}
       </div>
     );
   }
 }
-export default AttendingEvents;
+export default UserEvents;
