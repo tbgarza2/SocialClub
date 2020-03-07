80d1230d7f4d97b78ae2f67e66f2ddea38f13a9e
NaseerHines <Hinesnaseer@gmail.com>
2020-03-07 14:13:55 -0600

    added boostrap for buttons for a username

--- client/src/Components/UserDetail.jsx
+++ client/src/Components/UserDetail.jsx
@@ -1,9 +1,20 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 
-const UserDetail = ({ user, display }) => (
+const UserDetail = ({ holdClickedUser, user, display }) => (
   <div>
-    <li style={{ listStyleType: 'none' }}>{user.name}</li>
-    <button onClick={display}>Profile</button>
+    <div>
+      <button type="button" className="btn btn-info">
+        <li
+          onClick={display}
+          holdClickedUser={holdClickedUser}
+          style={{ listStyleType: "none" }}
+        >
+          {user.name}
+        </li>
+      </button>
+    </div>
+    <br />
   </div>
 );
 
