03600e20debb93fea2d79b2a8f4ce86a9ad4f44f
William Steck <steckwilliam@gmail.com>
2020-03-07 13:44:23 -0600

    Added dynamic event routes

--- client/src/Components/UserEvents.jsx
+++ client/src/Components/UserEvents.jsx
@@ -1,44 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class UserEvents extends Component {
   constructor(props) {
     super(props);
     this.State = {
-      events: [],
+      userEvents: [],
     };
   }
 
-  UNSAFE_componentWillMount() {
-    if (localStorage.getItem('events')) {
-      this.setState({
-        events: JSON.parse(localStorage.getItem('events')),
-      });
-    }
-  }
+  // UNSAFE_componentWillMount() {
+  //   if (localStorage.getItem('userEvents')) {
+  //     this.setState({
+  //       events: JSON.parse(localStorage.getItem('userEvents')),
+  //     });
+  //   }
+  // }
 
-  componentDidMount() {
-    if (!localStorage.getItem('events')) {
-      this.setState({
-        events: this.props.events,
-      });
-    }
-  }
+  // componentDidMount() {
+  //   if (!localStorage.getItem('userEvents')) {
+  //     this.setState({
+  //       events: this.props.userEvents,
+  //     });
+  //   }
+  // }
 
-  UNSAFE_componentWillUpdate(nextProps, nextState) {
-    localStorage.setItem('events', JSON.stringify(nextState.events));
-  }
+  // UNSAFE_componentWillUpdate(nextProps, nextState) {
+  //   localStorage.setItem('userEvents', JSON.stringify(nextState.events));
+  // }
 
   render() {
-    const events = this.props.events || this.state.events;
+    const events = this.props.userEvents || this.state.userEvents;
     return (
       <div>
         <h3>Events You Created</h3>
         <ul>
-          {events.map(event => (
+          {events.map((event, index) => (
+            <Link to={{
+              pathname: `/${event.id}`,
+              state: {
+                eventID: event.id,
+              },
+            }}
+            >
             <li key={event.id} id={event.id} onClick={this.props.handleClick}>
               {event.name}
             </li>
+            </Link>
           ))}
         </ul>
       </div>
